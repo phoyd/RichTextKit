b233b3e3f573b4480e9b7fd9f6aed502ea1385d7
Marco Manfredini <git@phoyd.net>
2026-06-10 17:37:21 +0200

    fixed test data path for Linux/macOS

--- Topten.RichTextKit.Test/BidiTest.cs
+++ Topten.RichTextKit.Test/BidiTest.cs
@@ -34,7 +34,7 @@ namespace Topten.RichTextKit.Test
 
              // Read the test file
             var location = System.IO.Path.GetDirectoryName(typeof(BidiTest).Assembly.Location);
-            var lines = System.IO.File.ReadAllLines(System.IO.Path.Combine(location, "TestData\\BidiTest.txt"));
+            var lines = System.IO.File.ReadAllLines(System.IO.Path.Combine(location, "TestData","BidiTest.txt"));
 
             var bidi = new Bidi();
 
